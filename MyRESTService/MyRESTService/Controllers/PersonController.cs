54c0a50efe6caf9070173819b54097b830bd37f6
ashish2377 <43794114+ashish2377@users.noreply.github.com>
2018-10-02 14:19:41 -0500

    Add files via upload

---
+++ MyRESTService/MyRESTService/Controllers/PersonController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MyRESTService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Person")]
+    public class PersonController : Controller
+    {
+        // GET: api/Person
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "Ashish", "Amit","Shubham", "Aashay" };
+        }
+
+        // GET: api/Person/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            var strArray = new[] { "Ashish", "Amit", "Shubham", "Aashay" };
+            return strArray[id];
+        }
+        
+        // POST: api/Person
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Person/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MyRESTService/MyRESTService/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MyRESTService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/User")]
+    public class UserController : Controller
+    {
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/User
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
